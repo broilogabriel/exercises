e8d5a2b9924b6483cb36fd98ad7d81a0b87f1b77
Gabriel Broilo <broilo.gabriel@gmail.com>
2017-05-18 21:10:38 +0100

    Create new project

---
+++ time_series/build.sbt
@@ -0,0 +1,5 @@
+name := "time_series"
+
+version := "17.5.1"
+
+scalaVersion := "2.12.2"
---
+++ time_series/src/main/scala/com/broilogabriel/Main.scala
@@ -0,0 +1,8 @@
+package com.broilogabriel
+
+/**
+  * Created by broilogabriel on 5/18/2017.
+  */
+class Main {
+
+}
